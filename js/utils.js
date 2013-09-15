d83a5872c079da34caaa9254f074e57c6f13e743
npn <kanadenipun@gmail.com>
2013-09-15 12:18:54 +0530

    upload form working

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,7 @@ var makediv=function(classname, content, parent){
 
 	var element=document.createElement("div");
 	element.className = classname;
+        txt = document.createTextNode(content)
 	element.innerHTML = content;
 	parent.appendChild(element);
 	return element;
